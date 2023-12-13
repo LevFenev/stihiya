c4bf10ffee9a575f8c105d66dd8986b6bc54791c
LevFenev <levfen@gmail.com>
2023-12-13 23:42:06 +0300

    day 2

--- public_html/index.php
+++ public_html/index.php
@@ -5,12 +5,28 @@ $s = "My";
 //print 'Hello $s Wild!';
 
 $s = 1;
+$g = new Greeting("Wild");
+$g1 = new Greeting(77);
 while ($s <= 10){
-    print_greeting("Wild");
-    print_greeting(77);
+    $g->print_greeting();
+    $g1->print_greeting();
     $s++;
 }
 
-function print_greeting(string $name) {
-    print 'hello '.$name.'!'."<br>";
+
+class Greeting {
+    private string $name;
+
+    /**
+     * Greeting constructor.
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function print_greeting():void {
+        print 'hello '.$this->name.'!'."<br>";
+    }
 }
