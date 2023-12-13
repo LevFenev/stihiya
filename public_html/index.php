41cd0c3f3c9bb3f78490415d61306f43b5cf0d35
LevFenev <levfen@gmail.com>
2023-12-13 23:24:51 +0300

    day 2

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,11 @@
 <?php
 $s = "My";
-print "Hello $s Wild!";
-print "<br>";
-print 'Hello $s Wild!';
+//print "Hello $s Wild!";
+//print "<br>";
+//print 'Hello $s Wild!';
+
+$s = 1;
+while ($s <= 10){
+    print 'Hello Wild!';
+    $s++;
+}
