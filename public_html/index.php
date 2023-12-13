5bfe5165581df7600faea88802b338dfc817dc90
LevFenev <levfen@gmail.com>
2023-12-14 01:08:55 +0300

    day 2

--- public_html/index.php
+++ public_html/index.php
@@ -5,17 +5,17 @@ $s = "My";
 //print 'Hello $s Wild!';
 
 $s = 1;
-$g = new Greeting("Wild");
-$g1 = new Greeting(77);
+$g = new Wild_Greeting();
+$g1 = new SeventySeven();
 while ($s <= 10){
     $g->print_greeting();
     $g1->print_greeting();
+    $g1->set_greeting($s);
     $s++;
 }
 
-
-class Greeting {
-    private string $name;
+abstract class Greeting implements DoGreeting {
+    protected string $name;
 
     /**
      * Greeting constructor.
@@ -29,4 +29,48 @@ class Greeting {
     public function print_greeting():void {
         print 'hello '.$this->name.'!'."<br>";
     }
+}
+
+class Wild_Greeting extends Greeting {
+
+    /**
+     * Wild_Greeting constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct("Wild");
+    }
+
+    public function set_greeting($s = "Wild")
+    {
+        $this->name = $s;
+    }
+}
+
+class SeventySeven extends Greeting {
+    use UsefulFunctions;
+
+    /**
+     * SeventySeven constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(77);
+    }
+
+    public function set_greeting($s)
+    {
+        $this->name = $s;
+    }
+}
+
+trait UsefulFunctions {
+    public function printNewLine() {
+        print "<br>";
+    }
+}
+
+interface DoGreeting {
+    public function set_greeting($s);
+
 }
