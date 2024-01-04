30e9d07e353d3e4933a00b4e825680c99d16da07
LevFenev <levfen@gmail.com>
2024-01-04 21:48:53 +0300

    fix 3 showing poem titles and collection titles

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Collection;
 use App\Models\Poem;
 use Illuminate\Http\Request;
 
 class CollectionController extends Controller
 {
     public function myPage() {
-        $collections = Poem::all();
+        $collections = Collection::all();
         return view('collection.list', ['collection'=>$collections]);
     }
 }
