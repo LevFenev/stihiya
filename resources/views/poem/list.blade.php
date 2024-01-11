b894c1c9680adf2a1151dc2c27ab245b9587ebd8
LevFenev <levfen@gmail.com>
2024-01-11 18:01:50 +0300

    creating poem links

--- resources/views/poem/list.blade.php
+++ resources/views/poem/list.blade.php
@@ -4,7 +4,7 @@
     <p>Стихи:
     <ol>
     @foreach($poems as $poem)
-        <li>{{$poem->title}}</li>
+        <li><a href="poems/{id}">{{$poem->title}}</a></li>
 
         @endforeach
     </ol>
