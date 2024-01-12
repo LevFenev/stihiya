8833c904b62d52827345538f98ff94a3858eac20
LevFenev <levfen@gmail.com>
2024-01-12 23:40:49 +0300

    attempting to make poem links from poem list 4

--- resources/views/poem/list.blade.php
+++ resources/views/poem/list.blade.php
@@ -4,7 +4,7 @@
     <p>Стихи:
     <ol>
     @foreach($poems as $poem)
-            <li><a href="poem.read/{{($poem->id)}}">{{($poem->title)}}</a></li>
+            <li><a href="poems/{{($poem->id)}}">{{($poem->title)}}</a></li>
         @endforeach
     </ol>
     </p>
