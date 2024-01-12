58c57b0c5b23d8cdd6c24638ab7e04bb1c385de6
LevFenev <levfen@gmail.com>
2024-01-12 23:23:28 +0300

    fixing brs one more time

--- resources/views/poem/read.blade.php
+++ resources/views/poem/read.blade.php
@@ -5,8 +5,8 @@
     @foreach($poems as $poem)
         <h2>{{($poem->title)}}</h2>
             <i>{{($poem->author_id)}}</i>
-    <p>{{str_replace("\n","<br>",($poem->content))}}</p>
-
+    <p>{!!str_replace("\n",'<br>',($poem->content))!!}</p>
+<br>
     <i>{{($poem->release_year)}}</i>
         @endforeach
 </div>
