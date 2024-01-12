f1d46cbd75a89c87654193aadcf4bd31c2b737f3
LevFenev <levfen@gmail.com>
2024-01-12 23:04:21 +0300

    showing poem in a column structure

--- resources/views/poem/read.blade.php
+++ resources/views/poem/read.blade.php
@@ -3,7 +3,7 @@
 <div>
     <p>Стихи:</p>
     @foreach($poems as $poem)
-        <h1>{{($poem->title)}}</h1>
+        <h2>{{($poem->title)}}</h2>
             <i>{{($poem->author_id)}}</i>
     <p>{{($poem->content)}}</p>
 
