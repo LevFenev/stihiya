f305d638723eb54ac1d05019e4f06759cce92643
LevFenev <levfen@gmail.com>
2024-01-19 22:59:17 +0300

    showing poems on personal collection page fix 1

--- resources/views/collection/read.blade.php
+++ resources/views/collection/read.blade.php
@@ -4,8 +4,8 @@
         <ol>
             @foreach($collections as $collection)
                 <h2>{{$collection->title}}</h2>
-                @foreach($poems)
-                    <p>({{$poems->title}}<br>)</p>
+                @foreach($poems as $poem)
+                    <p>({{$poem->title}}<br>)</p>
                     @endforeach
             @endforeach
         </ol>
