b19a185b8c7687ac0aeba4c5e669d56b337ae758
LevFenev <levfen@gmail.com>
2024-01-19 23:14:15 +0300

    showing poems on personal collection page fix 3 also optimizing

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -17,17 +17,20 @@ class CollectionController extends Controller
     public function readCollection(string $id) {
         $collections = Collection::where('id',$id)->get(); // данные записываются в переменную - скидывает полный объект
 
-        $poems = [];
+        $poem_ids = [];
 
         foreach ($collections as $collection) {
             // print $collection->poem_id;
             $collection_content = PoemCollectionLink::where('collection_id',$collection->id)->get();
 
             foreach ($collection_content as $collection_item) {
-                print $collection_item->poem_id;
-                $poems[] = Poem::where('id',$collection_item->poem_id)->get();
+                //print $collection_item->poem_id;
+                $poem_ids[] = $collection_item->poem_id;
+                // Poem::where('id',$collection_item->poem_id)->get();
             }
         }
+
+        $poems = Poem::whereIn('id',$poem_ids); // теперь для всех строчек один запрос / 19.01.24
 
         return view('collection.read',['collections'=>$collections, 'poems'=>$poems]); // переменная - poem
     }
--- resources/views/collection/read.blade.php
+++ resources/views/collection/read.blade.php
@@ -5,9 +5,7 @@
             @foreach($collections as $collection)
                 <h2>{{$collection->title}}</h2>
                 @foreach($poems as $poem)
-                    @foreach($poem as $single_poem)
-                        <p>({{$single_poem->title}}<br>)</p>
-                    @endforeach
+                        <p>({{$poem->title}}<br>)</p>
                     @endforeach
             @endforeach
         </ol>
