12c6c5941b30c1140dba5c7c130d09e6ae821769
LevFenev <levfen@gmail.com>
2024-01-25 20:55:11 +0300

    now showing users and poems fix 002

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Poem;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,5 +11,10 @@ class UserController extends Controller
     public function showUsers() {
         $users = User::all();
         return view('users.list', ['users'=>$users]);
+    }
+
+    public function showPoems() {
+        $poems = Poem::all();
+        return view('poems.list', ['poems'=>$poems]);
     }
 }
