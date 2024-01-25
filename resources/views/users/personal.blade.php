2c14e2425deb58f588aba47c0e0567bfcc49c69e
LevFenev <levfen@gmail.com>
2024-01-25 23:43:03 +0300

    showing not users poems showing their comments fix 010

--- resources/views/users/personal.blade.php
+++ resources/views/users/personal.blade.php
@@ -3,14 +3,12 @@
     <div>
         @foreach($users as $user)
         <p>Пользователь:{{$user->name}}</p>
-        <ol>
                 @foreach($poems as $poem)
                 @foreach($comments as $comment)
-                <li>{{$comment->content}}</li>
-                <li><a href="poems/{{($poem->id)}}">{{$poem->title}}</a></li>
+                {{$comment->content}}
+                <a href="poems/{{($poem->id)}}">{{$poem->title}}</a>
                 @endforeach
                 @endforeach
             @endforeach
-        </ol>
     </div>
 @endsection
