0538abed1052da54c53b51a1f66434406a4dc936
LevFenev <levfen@gmail.com>
2024-01-25 23:01:39 +0300

    showing not users poems showing their comments fix 004

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -1,13 +1,10 @@
 @extends('layout') <!-- откуда он получает базу данных? / Должен из UserController, но не получает -->
 @section('content')
     <div>
-        <p>Пользователи и их стихи:</p>
+        <p>Пользователи</p>
         <ol>
             @foreach($users as $user)
                 <h2>{{$user->username}}</h2>
-                @foreach($poems as $poem)
-                    <li>{{$poem->title}}</li> <a href="poems/{{($poem->id)}}"></a>
-                @endforeach
             @endforeach
         </ol>
     </div>
