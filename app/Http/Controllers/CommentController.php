d32dbc89f584dada30b575daa070c3b9da21566f
LevFenev <levfen@gmail.com>
2024-02-09 22:47:48 +0300

    fixing remove comment function 002

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,17 +10,17 @@ class CommentController extends Controller
 
     // по идее должно располагаться в comment controller
     public function deleteComment(string $id) {
-        $comments = Comment::where('comment_id',$id)->delete();
-        return view('comment.comment_delete', ['comments'=>$comments]);
+        $comments = Comment::where('id',$id)->delete();
+        return view('comment.delete', ['comments'=>$comments]);
     }
 
     public function restoreComment(string $id) {
-        $comments = Comment::where('comment_id',$id)->restore();
+        $comments = Comment::where('id',$id)->restore();
         return view('comment.restore', ['comments'=>$comments]);
     }
 
     public function showTrashedComments(string $id) {
-        $comments = Comment::where('comment_id',$id)->trashed();
+        $comments = Comment::where('id',$id)->trashed();
         return view('comment.trashed', ['comments'=>$comments]);
     }
 }
