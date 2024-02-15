d753dbec4d6b1c32d0590ebc3de5e49acc94cf8d
LevFenev <levfen@gmail.com>
2024-02-15 22:55:49 +0300

    made form a form

--- resources/views/comment/form.blade.php
+++ resources/views/comment/form.blade.php
@@ -1,7 +1,9 @@
 @extends('layout')
 @section('content')
+    <form action="/comment/post/2" method="post">
     <h3>Хотите оставить комментарий?</h3>
     <p>Текст комментария</p>
-    <textarea>
-    <a href="/comment/post/2">Отправить</a>
+        <textarea></textarea>
+    <button type="submit">Отправить</button>
+    </form>
 @endsection
