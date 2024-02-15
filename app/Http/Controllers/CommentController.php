c1365aadb270ba3648cbf2f870e6e4307699a476
LevFenev <levfen@gmail.com>
2024-02-15 22:26:17 +0300

    fixing redirect by adding slash

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -25,11 +25,11 @@ class CommentController extends Controller
         $toBeRestoredComment = Comment::withTrashed()->where('id',$id)->get(); //он даёт удалённые комментарии в виде массива
         $poem_id = $toBeRestoredComment[0]->poem_id; // 0 потому что массив
         $comments = Comment::where('id',$id)->restore(); // restore даёт кол-во id
-        return redirect()->route('poems', ['id'=>$poem_id]); // вернет на стих с которого удалили коммент
+        return redirect()->route('/poems', ['id'=>$poem_id]); // вернет на стих с которого удалили коммент
     }
 
     public function postComment(string $poem_id) {
         // как-то нужно написать что здесь остаётся коммент
-        return redirect()->route('poems', ['id'=>$poem_id]); // вернет на стих с которого удалили коммент
+        return redirect()->route('/poems', ['id'=>$poem_id]); // вернет на стих с которого удалили коммент
     }
 }
