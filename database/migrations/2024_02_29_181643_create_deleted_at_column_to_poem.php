d83d0b5852510a506231dc4f60babed93f2f3701
LevFenev <levfen@gmail.com>
2024-02-29 21:26:53 +0300

    added column deleted_at through migrate

--- database/migrations/2024_02_29_181643_create_deleted_at_column_to_poem.php
+++ database/migrations/2024_02_29_181643_create_deleted_at_column_to_poem.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('deleted_at_column_to_poem', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('poem', function(Blueprint $table){
+            $table->dateTime('deleted_at');
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('deleted_at_column_to_poem');
+        Schema::table('poem', function(Blueprint $table){
+            $table->dropColumn('deleted_at'); // dropColumn - убрать колонку из бд
+        });
     }
 };
