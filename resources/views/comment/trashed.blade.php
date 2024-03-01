2d5312f533c36dbf3c372d6af7504188e5db22bd
LevFenev <levfen@gmail.com>
2024-03-01 15:10:26 +0300

    added comment publication section on a trashed page.

--- resources/views/comment/trashed.blade.php
+++ resources/views/comment/trashed.blade.php
@@ -4,6 +4,7 @@
     @foreach($comments as $comment)
         <p>Пользователь: {{$comment->user_id}}</p>
         <p>Содержание: {{$comment->content}}</p>
+        <p>Дата публикации: {{$comment->created_at}}</p>
         <p>Дата удаления: {{$comment->deleted_at}}</p>
         <a href="restore/{{$comment->id}}">[Восстановить]</a>
         <br>
