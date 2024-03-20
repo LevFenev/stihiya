8b341b1bfa9ad9e534f047ec527be0d782bff9a0
LevFenev <levfen@gmail.com>
2024-03-20 16:10:00 +0300

    changing it so that it would take comments data and poems data from the model not the controller 3

--- resources/views/user/admin/list.blade.php
+++ resources/views/user/admin/list.blade.php
@@ -4,8 +4,14 @@
         <p>Пользователи</p>
         <ol>
             @foreach($users as $user)
-
                 <li><a href="users/{{$user->id}}">{{$user->name}}</a> [<a href="/admin/users/delete/{{$user->id}}">удалить</a>]</li>
+                @foreach($user->comments)
+                    <h3>{{$comment->username}}</h3>
+                    <p>{{$comment->created_at}}</p>
+                    <p>{{$comment->content}}</p>
+                    <p>{{$comment->poem_id}}</p>
+                    <p>Лайки: {{$comment->like_count}}</p>
+                @endforeach
             @endforeach
         </ol>
     </div>
