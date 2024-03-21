4c3e55489857443a33ad5716df03b681b3ab9a4d
LevFenev <levfen@gmail.com>
2024-03-21 21:43:26 +0300

    changed it so the actual title of the collection would show up on a page

--- resources/views/collection/collection2.blade.php
+++ resources/views/collection/collection2.blade.php
@@ -1,8 +1,8 @@
 @extends('layout')
 @section('content')
     <div>
-        <h2>Сборник "Пушкин. Избранное"</h2>
         @foreach($collections->poems as $poem)
+            <h2>{{$collection->title}}</h2>
             {{$poem->title}}
         @endforeach
     </div>
