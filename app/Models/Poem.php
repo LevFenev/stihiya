ab27d2996a4359ddd289a70b149e4c12ca24faf2
LevFenev <levfen@gmail.com>
2024-03-22 13:31:59 +0300

    trying to show an actual username instead of a author_id on a read poem page by changing it on a view: from $poem->author_id to $poem->user->name and adding user method (with belongsTo) to the Poem class

--- app/Models/Poem.php
+++ app/Models/Poem.php
@@ -22,4 +22,8 @@ class Poem extends Model
     public function comments() {
         return $this->hasMany(\App\Models\Comment::class);
     }
+
+    public function user() {
+        return $this->belongsTo(\App\Models\User::class); //$this->poem_id;
+    }
 }
--- resources/views/poem/read.blade.php
+++ resources/views/poem/read.blade.php
@@ -5,7 +5,7 @@
     <a href="/poems">Общий список стихов</a>
     @foreach($poems as $poem)
         <h2>{{($poem->title)}}</h2>
-            <i>{{($poem->author_id)}}</i> <!-- ($user->username)   как-то надо определять айдишник того кто написал стих и вывести имя -->
+            <i>{{($poem->user->name)}}</i> <!-- ($user->username)   как-то надо определять айдишник того кто написал стих и вывести имя -->
     <p>{!!str_replace("\n",'<br>',($poem->content))!!}</p>
 <br>
     <i>{{($poem->release_year)}}</i>
