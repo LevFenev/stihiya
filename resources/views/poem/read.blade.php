07f1252549228180709713002c9f18af7d9b4d82
LevFenev <levfen@gmail.com>
2024-03-22 13:49:56 +0300

    error due to null of some author_ids. changing it with an @if (isset($poem->user->name))

--- resources/views/poem/read.blade.php
+++ resources/views/poem/read.blade.php
@@ -5,7 +5,7 @@
     <a href="/poems">Общий список стихов</a>
     @foreach($poems as $poem)
         <h2>{{($poem->title)}}</h2>
-            <i>{{($poem->user->name)}}</i> <!-- ($user->username)   как-то надо определять айдишник того кто написал стих и вывести имя -->
+            <i>@if(isset($poem->user->name)) @endif</i> <!-- ($user->username)   как-то надо определять айдишник того кто написал стих и вывести имя -->
     <p>{!!str_replace("\n",'<br>',($poem->content))!!}</p>
 <br>
     <i>{{($poem->release_year)}}</i>
