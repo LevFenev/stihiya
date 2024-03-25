d68da867386161265ce3cb4850c195c28f23cf24
LevFenev <levfen@gmail.com>
2024-03-25 15:39:06 +0300

    quick fix: removed all the styles

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,20 +1,10 @@
 <div>
     <!-- Well begun is half done. - Aristotle -->
     <ul>
-        <li style="display: block; float: left">
-            <a href="/users">Пользователи</a>
-        </li>
-        <li style="display: block; float: left">
-            <a href="/poems">Стихи</a>
-        </li>
-        <li style="display: block; float: left">
-            <a href="/collections">Сборники</a>
-        </li>
-        <li style="display: block; float: left">
-            <a href="songs">Песни</a>
-        </li>
-        <li style="display: block; float: left">
-            <a href="/albums">Альбомы</a>
-        </li>
+        <li><a href="/users">Пользователи</a></li>
+        <li><a href="/poems">Стихи</a></li>
+        <li><a href="/collections">Сборники</a></li>
+        <li><a href="songs">Песни</a></li>
+        <li><a href="/albums">Альбомы</a></li>
     </ul>
 </div>
