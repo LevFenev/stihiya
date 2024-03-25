95fd01fe9d35396ac7f7ef4272a3a195f219e01b
LevFenev <levfen@gmail.com>
2024-03-25 21:51:12 +0300

    checking author name appearance on the page 4 added round brackets to the username method on a poem read view

--- resources/views/poem/read.blade.php
+++ resources/views/poem/read.blade.php
@@ -5,7 +5,7 @@
     <a href="/poems">Общий список стихов</a>
     @foreach($poems as $poem)
         <h2>{{($poem->title)}}</h2><h3>Фёдор Тютчев</h3>
-            <i>{{$poem->username}}</i>
+            <i>{{$poem->username()}}</i>
     <p>{!!str_replace("\n",'<br>',($poem->content))!!}</p>
 <br>
     <i>{{($poem->release_year)}}</i>
