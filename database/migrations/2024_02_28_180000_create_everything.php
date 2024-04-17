70bfae4f555057030ef4ea07bbade7906239564c
LevFenev <levfen@gmail.com>
2024-04-17 14:02:05 +0300

    created migration create_deleted_at_column_at_song

--- database/migrations/2024_02_28_180000_create_everything.php
+++ database/migrations/2024_02_28_180000_create_everything.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user', function (Blueprint $table) {
+        /*Schema::create('user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
 
@@ -153,7 +153,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->string('status_name');
-        });
+        });*/
     }
 
     /**
