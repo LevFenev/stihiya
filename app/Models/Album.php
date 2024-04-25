e59dd7110bdb917dfb618a557c8f6927b2fa8dc8
LevFenev <levfen@gmail.com>
2024-04-25 16:02:56 +0300

    added attributes for an album

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -10,4 +10,16 @@ class Album extends Main
     use HasFactory;
 
     protected $table = 'album';
+    protected $attributes = [ // to be changed after db update #tbcadu
+        'id'=>'0',
+        'title'=>'Без названия',
+        'author'=>'Имя артиста',
+        'release_year'=>'2024',
+        'release_date'=>'11.01.2024',
+        'file'=>'',
+    ];
+
+    public function songs() {
+        return $this->belongsToMany(\App\Models\Song::class,'song_album');
+    }
 }
