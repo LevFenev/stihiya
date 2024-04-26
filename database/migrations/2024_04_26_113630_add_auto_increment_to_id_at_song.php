db5586492c48466feb36dbf40e94a7d1acc6c5bb
LevFenev <levfen@gmail.com>
2024-04-26 14:42:23 +0300

    created migration add_auto_increment_to_id_at_song and added change() to it

--- database/migrations/2024_04_26_113630_add_auto_increment_to_id_at_song.php
+++ database/migrations/2024_04_26_113630_add_auto_increment_to_id_at_song.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('song', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->change();
         });
     }
 
