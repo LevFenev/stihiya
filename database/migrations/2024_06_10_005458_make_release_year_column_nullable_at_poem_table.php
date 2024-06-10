c38527d042d30da9b410bfeff5da9748cc7fc2dc
LevFenev <levfen@gmail.com>
2024-06-10 03:59:10 +0300

    pushing migration make_release_year_column_nullable_at_poem_table

--- database/migrations/2024_06_10_005458_make_release_year_column_nullable_at_poem_table.php
+++ database/migrations/2024_06_10_005458_make_release_year_column_nullable_at_poem_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
+        Schema::table('poem', function (Blueprint $table) {
+            $table->integer('release_year')->nullable()->change();
+        });
     }
 
     /**
@@ -19,6 +20,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('poem', function (Blueprint $table) {
+            $table->dropColumn('release_year');
+            $table->integer('release_year');
+        });
     }
 };
